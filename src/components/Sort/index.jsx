90e4691a6a5a9019359e0096347eade9deb40836
The73756 <the73756the@gmail.com>
2022-07-29 14:48:14 +0900

    fix sort markers

--- src/components/Sort/index.jsx
+++ src/components/Sort/index.jsx
@@ -6,12 +6,12 @@ export default function Sort({ value, onChangeSort }) {
   const [isOpen, setIsOpen] = useState(false);
 
   const list = [
-    { name: 'популярности 🡻', sortProperty: 'rating', order: 'desc' },
-    { name: 'популярности 🡹', sortProperty: 'rating', order: 'asc' },
-    { name: 'алфавиту 🡹', sortProperty: 'title', order: 'asc' },
-    { name: 'алфавиту 🡻', sortProperty: 'title', order: 'desc' },
-    { name: 'цене 🡹', sortProperty: 'price', order: 'asc' },
-    { name: 'цене 🡻', sortProperty: 'price', order: 'desc' },
+    { id: 0, name: 'популярности', reverseIcon: true, sortProperty: 'rating', order: 'desc' },
+    { id: 1, name: 'популярности', sortProperty: 'rating', order: 'asc' },
+    { id: 2, name: 'алфавиту', sortProperty: 'title', order: 'asc' },
+    { id: 3, name: 'алфавиту', reverseIcon: true, sortProperty: 'title', order: 'desc' },
+    { id: 4, name: 'цене', sortProperty: 'price', order: 'asc' },
+    { id: 5, name: 'цене', reverseIcon: true, sortProperty: 'price', order: 'desc' },
   ];
 
   const handleClickPopup = () => {
@@ -41,16 +41,42 @@ export default function Sort({ value, onChangeSort }) {
           />
         </svg>
         <b>Сортировка по:</b>
-        <span>{value.name}</span>
+        <span>
+          {value.name}
+          <svg
+            xmlns='http://www.w3.org/2000/svg'
+            className={styles.icon}
+            style={{ transform: `rotate(${value.reverseIcon ? 180 : 0}deg)` }}
+            width='10'
+            height='10'
+            viewBox='0 0 464 464'>
+            <path
+              d='M416.483,265.441c-2.704-2.777-6.419-4.336-10.295-4.321h-75.52c-5.891,0-10.667-4.776-10.667-10.667V14.293    C320.001,6.399,313.602,0,305.708,0H158.721c-7.894,0-14.293,6.399-14.293,14.293v236.16c0,5.891-4.776,10.667-10.667,10.667    H58.028c-7.893-0.149-14.412,6.128-14.561,14.02c-0.075,3.951,1.49,7.757,4.321,10.513l174.08,174.08    c2.648,2.69,6.252,4.224,10.027,4.267c3.849,0.014,7.54-1.524,10.24-4.267l174.08-174.08    C421.87,280.146,421.99,271.097,416.483,265.441z'
+              fill='#000000'
+              data-original='#000000'></path>
+          </svg>
+        </span>
       </div>
       <div className={styles.popup}>
         <ul>
           {list.map((obj, index) => (
             <li
-              className={value.name === obj.name ? styles.active : ''}
+              className={value.id === obj.id ? styles.active : ''}
               key={index}
               onClick={() => handleClickSelect(obj)}>
               {obj.name}
+              <svg
+                xmlns='http://www.w3.org/2000/svg'
+                className={styles.icon}
+                style={{ transform: `rotate(${obj.reverseIcon ? 180 : 0}deg)` }}
+                width='10'
+                height='10'
+                viewBox='0 0 464 464'>
+                <path
+                  d='M416.483,265.441c-2.704-2.777-6.419-4.336-10.295-4.321h-75.52c-5.891,0-10.667-4.776-10.667-10.667V14.293    C320.001,6.399,313.602,0,305.708,0H158.721c-7.894,0-14.293,6.399-14.293,14.293v236.16c0,5.891-4.776,10.667-10.667,10.667    H58.028c-7.893-0.149-14.412,6.128-14.561,14.02c-0.075,3.951,1.49,7.757,4.321,10.513l174.08,174.08    c2.648,2.69,6.252,4.224,10.027,4.267c3.849,0.014,7.54-1.524,10.24-4.267l174.08-174.08    C421.87,280.146,421.99,271.097,416.483,265.441z'
+                  fill='#000000'
+                  data-original='#000000'></path>
+              </svg>
             </li>
           ))}
         </ul>
