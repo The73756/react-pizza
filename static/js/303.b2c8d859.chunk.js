c4103b598d939f323015ab21aa2e3bd8d6d1ac84
The73756 <the73756the@gmail.com>
2022-08-16 10:16:10 +0900

    Updates

---
+++ static/js/303.b2c8d859.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_pizza=self.webpackChunkreact_pizza||[]).push([[303],{8303:function(e,a,t){t.r(a);var u=t(4890),c=t(184);a.default=function(){return(0,c.jsx)(u.iv,{})}}}]);
+//# sourceMappingURL=303.b2c8d859.chunk.js.map
